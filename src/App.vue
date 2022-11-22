584350f79dc8a4abee5afa52fd177344c3f4dee6
Gil Cunha <gilcunha@outlook.pt>
2022-11-22 16:54:52 +0000

    first-commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Dog breed="bulldog" />
+  </div>
+</template>
+
+<script>
+import Dog from "./components/Dog.vue";
+
+export default {
+  name: "App",
+  components: {
+    Dog,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Dog.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <button @click="getDog">GET a {{ breed }}</button>
+    <img :src="dog.message" />
+  </div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Dog",
+  props: {
+    breed: String,
+  },
+  data() {
+    return {
+      dog: "",
+    };
+  },
+  methods: {
+    getDog: async function () {
+      const response = await fetch(
+        `https://dog.ceo/api/breed/${this.breed}/images/random`
+      );
+      this.dog = await response.json();
+    },
+  },
+};
+</script>
+
+<style scoped></style>
